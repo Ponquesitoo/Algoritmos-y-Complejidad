8d5b48284f8fab7023772a9d8515b4b5a38959de
Ponquesitoo <130908036+Ponquesitoo@users.noreply.github.com>
2024-09-10 23:13:42 -0300

    Update dataSets.cpp

--- Tarea 1/Data Sets/dataSets.cpp
+++ Tarea 1/Data Sets/dataSets.cpp
@@ -48,6 +48,19 @@ std::vector<int> generarParcialmenteOrdenado(int tamanio, double porcentajeOrden
     return datos;
 }
 
+// Función para generar una matriz aleatoria
+std::vector<std::vector<int>> generarMatriz(int filas, int columnas, std::mt19937 &gen, int min = 0, int max = 10) {
+    std::vector<std::vector<int>> matriz(filas, std::vector<int>(columnas));
+    std::uniform_int_distribution<> dis(min, max);
+
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            matriz[i][j] = dis(gen);
+        }
+    }
+    return matriz;
+}
+
 // Función para guardar datos en un archivo de texto
 void guardarDatos(const std::vector<int> &datos, const std::string &nombreArchivo) {
     std::ofstream archivo(nombreArchivo);
@@ -62,10 +75,32 @@ void guardarDatos(const std::vector<int> &datos, const std::string &nombreArchiv
     }
 }
 
+// Función para guardar una matriz en un archivo de texto
+void guardarMatriz(const std::vector<std::vector<int>> &matriz, const std::string &nombreArchivo) {
+    std::ofstream archivo(nombreArchivo);
+    if (archivo.is_open()) {
+        for (const auto &fila : matriz) {
+            for (const auto &num : fila) {
+                archivo << num << " ";
+            }
+            archivo << std::endl;
+        }
+        archivo.close();
+        std::cout << "Matriz guardada en " << nombreArchivo << std::endl;
+    } else {
+        std::cerr << "No se pudo abrir el archivo " << nombreArchivo << std::endl;
+    }
+}
+
 int main() {
-    int tamanio = 1000000; // Puedes variar el tamanio según necesites
+    int tamanio = 1000000; // tamanio de datasets ordenamiento de datos.
+    int cuadrada = 5000; // tamanio de las matrices
+    
+    // Inicializar generador de números aleatorios 
+     std::mt19937 gen(static_cast<unsigned int>(time(0)));
 
     // Generar y guardar datasets
+    // ordenamiento de datos
     std::vector<int> aleatorio = generarAleatorio(tamanio);
     guardarDatos(aleatorio, "aleatorio.txt");
 
@@ -77,6 +112,13 @@ int main() {
 
     std::vector<int> parcialmenteOrdenado = generarParcialmenteOrdenado(tamanio, 0.5);
     guardarDatos(parcialmenteOrdenado, "parcialmente_ordenado.txt");
+
+    // multiplicación de matrices
+    std::vector<std::vector<int>> matriz1 = generarMatriz(cuadrada, cuadrada, gen);
+    guardarMatriz(matriz1, "m1.txt");
+
+    std::vector<std::vector<int>> matriz2 = generarMatriz(cuadrada, cuadrada, gen);
+    guardarMatriz(matriz2, "m2.txt");
 
     return 0;
 }
